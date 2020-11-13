6866e71344ba17da2a09aacff0136c63d9af0155
Ernie Peterson <ernie.peterson05@gmail.com>
2020-11-13 13:48:38 -0500

    Updated test names to better conform with best practices

--- FizzBuzz/LogicTests/LogicTests.cs
+++ FizzBuzz/LogicTests/LogicTests.cs
@@ -9,28 +9,28 @@ namespace LogicTests
     {
         [TestMethod]
         // Test num % 3 = Fizz
-        public void Logic_FizzTest()
+        public void Logic_Three_Returns_Fizz()
         {
             Assert.AreEqual(Logic.FizzBuzz(3), "Fizz");
         }
 
         [TestMethod]
         // Test num % 3 = Buzz
-        public void Logic_BuzzTest()
+        public void Logic_Five_Returns_Buzz()
         {
             Assert.AreEqual(Logic.FizzBuzz(5), "Buzz");
         }
 
         [TestMethod]
         // Test num % 3 & num % 5 = FizzBuzz
-        public void Logic_FizzBuzzTest()
+        public void Logic_Fifteen_Returns_FizzBuzz()
         {
             Assert.AreEqual(Logic.FizzBuzz(15), "FizzBuzz");
         }
 
         [TestMethod]
         // Random tests - change x for number of random tests
-        public void Logic_RandTest()
+        public void Logic_RandomNumber_Returns_Correct_Response()
         {
             int x = 50;
             int count = 0;
